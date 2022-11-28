aaa89fe2f0e802c4df92630101c8e91201fc50ee
Balaji R <balajilp003@gmail.com>
2022-11-28 18:06:42 +0530

    adding before and after in LL part 2 updated

--- Add_DLL_part2.py
+++ Add_DLL_part2.py
@@ -4,9 +4,10 @@ class Node:
         self.nref = None 
         self.pref = None 
 
+
 class Doubly_LL:
     def __init__(self):
-        self.head = None
+        self.head = None 
 
     def print_all(self):
         if self.head is None:
@@ -18,6 +19,7 @@ class Doubly_LL:
                 n = n.nref 
     
     def print_reverse_all(self):
+
         if self.head is None:
             print("linked list is empty")
         else:
@@ -26,8 +28,9 @@ class Doubly_LL:
                 n = n.nref 
 
             while n is not None:
-                print(n.data)
+                print(n.data) 
                 n = n.pref
+     
 
     def insert_empty(self, data):
         if self.head is None:
@@ -57,11 +60,53 @@ class Doubly_LL:
             n.nref = new_node 
             new_node.pref = n
 
-d = Doubly_LL()
+    def add_after(self, data, x):
+        if self.head is None:
+            print("Linked list empty!!!")
+        else:
+            n = self.head 
+            while n is not None:
+                if x == n.data:
+                    break 
+                n = n.nref 
+            if n is None:
+                print("given node is not present in the entire linked list")
+            else:
+                new_node = Node(data)
+                new_node.nref = n.nref 
+                new_node.pref = n 
+                if n.nref is not None:
+                    n.nref.pref = new_node
+                n.nref = new_node 
+
+    def add_before(self, data, x):
+        if self.head is None:
+            print("Linked list empty!!!")
+        else:
+            n = self.head 
+            while n is not None:
+                if x == n.data:
+                    break 
+                n = n.nref 
+            if n is None:
+                print("given node is not present in the entire linked list")
+            else:
+                new_node = Node(data)
+                new_node.nref = n 
+                new_node.pref = n.pref 
+                if n.pref is not None:
+                    n.pref.nref = new_node 
+                else:
+                    self.head = new_node 
+                n.pref = new_node 
+
+
+
+d = Doubly_LL()    
 
 d.add_begin(10)
 d.add_begin(20)
 d.add_end(40)
 d.add_end(50)
+d.add_after(25, 20)
 d.print_all()
-
--- Inflation_rate.py
+++ Inflation_rate.py
@@ -1,8 +1,9 @@
-salary = 4000000
-slip = []
-for i in range(8):
-    salary = salary + ((salary*10)/100)
-    slip.append(salary)
-print(salary)
-print(sum(slip))
-
+amount = 1050 
+lst = []
+for i in range(36):
+    amount = amount-30
+    lst.append(amount)
+print(lst)
+print(sum(lst))
+print(105000+sum(lst))
+print(105000+36000)
