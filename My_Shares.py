1ebefc38624ecd8f2400b9fbace007a13ee200a6
Balaji R <balajilp003@gmail.com>
2022-12-16 12:04:31 +0530

    my finance file which contains what all are the diff stocks that i have in portfolio

---
+++ My_Shares.py
@@ -0,0 +1,9 @@
+shares = {"DIVI'S LAB":{"DATE_OF_BUY":"01-11-2022", "Quantity":9, "BUY_PRICE":3698, "BUY_BROKERAGE":0, "STT_CHARGE":33, "OTHER_CHARGE":6.35, "TOTAL_PRICE":33282, "TOTAL_BUY_PRICE_INCLUDING_TAX": 33321.35, },
+           "MANAPPURAM":{},
+           "MUTHOOT FINANCE":{},
+           "KALYAN_JEWELLERS":{},
+           "TMB":{},
+           "WIPRO":{},
+           "TECHMAHINDRA":{}
+           }
+print(shares["DIVI'S LAB"]["DATE_OF_BUY"])
